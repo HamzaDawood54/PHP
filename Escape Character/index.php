26f8782b4afae8aef9231cf8b2dffed4b2b350bb
HamzaDawood54 <99987231+HamzaDawood54@users.noreply.github.com>
2025-05-30 13:13:57 +0500

    Add files via upload

---
+++ Escape Character/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+// PHP will raise an error when executing this statement
+$x="We are the so-called "Vikings" from the north.";
+echo $x;
+?>
+</body>
+</html>
