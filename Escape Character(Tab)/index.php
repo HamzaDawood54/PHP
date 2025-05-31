a889526b24fd291330186be4023e7aa6e48bbb1b
HamzaDawood54 <99987231+HamzaDawood54@users.noreply.github.com>
2025-05-31 20:25:00 +0500

    Add files via upload

---
+++ Escape Character(Tab)/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<pre>
+<?php
+$x="Hello\tWorld";
+echo $x;
+?>
+</pre>
+<p>To preserve any whitespace or line breaks, we have wrapped the results in a PRE element.</p>
+<body>
+</html>
