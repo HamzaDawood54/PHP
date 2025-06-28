2d5a83ca44b58d2e212367d71e1149a83b7fce28
HamzaDawood54 <99987231+HamzaDawood54@users.noreply.github.com>
2025-06-28 21:56:05 +0500

    Add files via upload

---
+++ PHP Magic Constants(__METHOD__)/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+class Fruits{
+    public function myValue(){
+        return __METHOD__;
+    }
+}
+$kiwi=new Fruits();
+echo $kiwi->myValue();
+?>
+</body>
+</html>
