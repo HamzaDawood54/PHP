0966382a4d8716824c8d2ef72c5e58f96bc29470
HamzaDawood54 <99987231+HamzaDawood54@users.noreply.github.com>
2025-06-29 20:53:07 +0500

    Add files via upload

---
+++ PHP Magic Constants(ClassName class)/index.php
@@ -0,0 +1,17 @@
+<?php
+namespace myArea;
+class Fruits{
+    public function myValue(){
+        return Fruits::class;
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$kiwi=new Fruits();
+echo $kiwi->myValue();
+?>
+</body>
+</html>
