e99019070c469153248774cf09090623e181a929
HamzaDawood54 <99987231+HamzaDawood54@users.noreply.github.com>
2025-06-29 20:46:56 +0500

    Add files via upload

---
+++ PHP Magic Constants(__TRAIT__)/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+trait message1{
+    public function msg1(){
+        echo __TRAIT__;
+    }
+}
+class Welcome{
+    use message1;
+}
+$obj=new Welcome();
+$obj->msg1();
+?>
+</body>
+</html>
