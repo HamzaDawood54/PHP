2a5a98103ea2df951f7258ff486db2b3d6b533cc
HamzaDawood54 <99987231+HamzaDawood54@users.noreply.github.com>
2025-07-01 16:26:49 +0500

    Add files via upload

---
+++ PHP Logical Operators(xor)/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>The xor Operator</h1>
+<p>Write a message if one of the conditions are true, but not if both conditions are true.</p>
+<?php
+$x=100;
+$y=50;
+if($x==100 xor $y==80){
+    echo "Hello World!";
+}
+?>
+</body>
+</html>
