491c7178019ea10c1338c03829760b5b023c64c0
HamzaDawood54 <99987231+HamzaDawood54@users.noreply.github.com>
2025-07-01 16:37:57 +0500

    Add files via upload

---
+++ PHP Logical Operators(!)/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>The ! Operator</h1>
+<p>Write a message if the condition is not true.</p>
+<?php
+$x=100;
+if(!($x==90)){
+    echo "Hello World!";
+}
+?>
+</body>
+</html>
